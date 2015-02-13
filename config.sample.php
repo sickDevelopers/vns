f78a19e8bd15c3dc34b3b91bf3c078461f6890a6
Oscar Chinellato <oscar.chinellato@sickdevelopers.com>
2015-02-13 15:36:33 +0100

    fix defines

--- config.sample.php
+++ config.sample.php
@@ -2,8 +2,8 @@
 
 // settare e cambiare nome in config.php
 
-define(API_KEY, 'your_api_key');
-define(OAUTH_SECRET, 'your_api_secret');
+define('API_KEY', 'your_api_key');
+define('OAUTH_SECRET', 'your_api_secret');
 
-define(ADMIN_SECRET, 'your_admin_token');
-define(ADMIN_TOKEN_SECRET, 'your_admin_token_secret');
+define('ADMIN_SECRET', 'your_admin_token');
+define('ADMIN_TOKEN_SECRET', 'your_admin_token_secret');
